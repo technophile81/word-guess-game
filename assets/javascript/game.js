9c4b89afc9aa1122d473bfc8442242d326dbaa2f
Technophile <patricia.snodgrass@gmail.com>
2018-04-19 19:09:25 -0500

    updated JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,6 +7,7 @@ var hangman = {};
 
 // Display how many wins the user has (starts at 0).
 hangman.wins = 0;
+hangman.roundsPlayed = 0;
 
 // Display how many guesses are remaining (starts at max - 7). 
 // NOTE TO SELF: When declaring an anonymous function, add space between function and opening parenthesis.
@@ -18,9 +19,9 @@ hangman.newRound = function () {
 };
 
 hangman.startRound = function () {
-
+    this.roundsPlayed++;
     this.roundStarted = true; // This starts the game.
-}
+};
 
 // Select a new word from an array. 
 hangman.selectNewWord = function () {
@@ -37,42 +38,64 @@ hangman.selectNewWord = function () {
     this.currentWord = allWords[Math.floor(Math.random() * allWords.length)]; // Current word is randomly generated through this formula
 
     return this.currentWord; // Return the currentWord variable/attribute.
-}
+};
 
 // Return the word current word / letters typed containing a score where the underscores has or hasn't been typed; for every character in the word, check if it's in if it's typed; if not,put an underscore
-hangman.getWordDisplay = function () { 
-    
+hangman.getWordDisplay = function () {
+
     var displayWord = ""; // This variable will contain a returned display.
 
     for (var i = 0; i < this.currentWord.length; i++) { // This accesses each letter in order.
 
+        //if (displayWord.length > 0) { // This adds a space between each letter or underscore, except at the beginning of the word. So first time it loops, it will not execute.
+        //        displayWord += " ";
+        //    }
         var letter = this.currentWord.charAt(i);  // Variable is assigning the current character indexed by i.
-        
+
         if (this.lettersTyped.includes(letter) || this.wrongGuessesRemaining < 1) { // This will show the answer when user runs out of guesses
             displayWord += letter.toUpperCase(); // displayWord letters are uppercase
-            
+
         } else {
             displayWord += "_"; // This replaces the letter with the underscore. This should be default because user has not yet pressed keys.
+
         }
     }
     return displayWord;
-}
+};
 
 // Updates the display to reflect the current state of the game
-hangman.updateDisplay = function () {  
+hangman.updateDisplay = function () {
     document.getElementById("hangman-wins").innerHTML = "You have won " + "<br/>" + this.wins + " times"; // + " / " + this.totalRounds; // incrememnt totalRounds in startRound
     document.getElementById("hangman-guesses").innerHTML = "Guesses remaining" + "<br/>" + this.wrongGuessesRemaining;
     document.getElementById("hangman-letters").innerHTML = "Current Word: " + "<br/>" + this.getWordDisplay();
     document.getElementById("hangman-typed").innerHTML = "Letters used: " + "<br/>" + this.lettersTyped.join(""); // This method on an array will by default concatenate all items with a comma, but it takes one argument to override this to be any other string to use as the glue - including an empty string.
+    if (this.roundsPlayed < 1) {
+        document.getElementById("hangman-status").innerHTML = "Welcome! Press space bar to play "; 
+    } else if (this.roundWon()) {
+        document.getElementById("hangman-status").innerHTML = "Congratulations! Hit Space Bar to play again!";
+    } else if (this.roundLost()) {
+        document.getElementById("hangman-status").innerHTML = "You suck. Hit Space Bar to play again!";
+    } else { 
+        document.getElementById("hangman-status").innerHTML = "Good luck!";
+    }
+};
 
-}
+hangman.roundLost = function () {
+    return (this.wrongGuessesRemaining < 1); 
+};
+
+hangman.roundWon = function () {
+    return (!this.roundLost() && !this.getWordDisplay().includes("_"));
+};
 
 // Invokes handleKey
-hangman.handleKey = function (key) {  
+hangman.handleKey = function (key) {
 
     if (!this.roundStarted) {
         if (key === " ") {
-            // TODO: If this is NOT the first round, invoke newRound before startRound
+            if (this.roundsPlayed > 0) {
+                this.newRound();
+            }
             this.startRound();
             this.updateDisplay();
         }
@@ -87,27 +110,34 @@ hangman.handleKey = function (key) {
 
             if (!this.currentWord.includes(key)) { // If user inputs wrong guess, wrong guess count goes down.
                 this.wrongGuessesRemaining--;
-                // TODO: if they lost, set roundStarted to false           
-            } else if (!this.getWordDisplay().includes("_")) {
+                if (this.roundLost()) { 
+                    this.roundStarted = false;
+                }           
+            } else if (this.roundWon()) {
                 this.wins++;
+                
                 this.roundStarted = false;
             }
         }
-
         this.updateDisplay();
     }
-}
+};
 
+hangman.handleReveal = function () {
+    this.wrongGuessesRemaining = 0;
+    this.roundStarted = false;
+    this.updateDisplay();   
+};
 
 // A function to handle the keyPress. 
-function onReady () {
-    document.onkeyup = function (event) { // callback to be invoked when the onkeyup event occurs
-        hangman.handleKey(event.key); 
-    };
+document.onkeyup = function (event) { // callback to be invoked when the onkeyup event occurs
+    hangman.handleKey(event.key);
+};
 
-    hangman.newRound();
-    hangman.updateDisplay();
-}
+document.getElementById("hangman-reveal").onclick = function (event) { 
+    hangman.handleReveal();
+};
 
-
+hangman.newRound();
+hangman.updateDisplay();
 
